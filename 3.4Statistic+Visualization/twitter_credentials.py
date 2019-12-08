21e37678b7caedce4a8ab0e33326bcb168daae35
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-08 10:17:47 -0500

    Create twitter_credentials.py

---
+++ 3.4Statistic+Visualization/twitter_credentials.py
@@ -0,0 +1,8 @@
+# register twitter client application(need a personal twitter account)
+# get 4 strings here: https://developer.twitter.com/en/apps
+# do not change the sequence
+# do not show to others
+KEY = ''
+KEY_SECRET = ''
+TOKEN = '' # this is a key
+TOKEN_SECRET = '' # this is a secret
