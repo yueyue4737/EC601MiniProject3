d81ffc0c1b2b28ba85881ac23c39d66510804886
Sophie Liu <46078971+yueyue4737@users.noreply.github.com>
2019-12-08 13:00:04 -0500

    Update twitter_credentials.py

--- 3.1tweepy+MySQL/twitter_credentials.py
+++ 3.1tweepy+MySQL/twitter_credentials.py
@@ -2,7 +2,10 @@
 # get 4 strings here: https://developer.twitter.com/en/apps
 # do not change the sequence
 # do not show to others
-KEY = ''
-KEY_SECRET = ''
-TOKEN = '' # this is a key
-TOKEN_SECRET = '' # this is a secret
+# copy yours in keys & token, once you need to use twitter API
+# If necessary, don't forget regenerating the keys and secrets
+
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
